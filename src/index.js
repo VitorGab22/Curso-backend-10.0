13c733ea3c7ac92593875e234caeb0bdef128cf6
Vitor Gabriel <75252725+VitorGab22@users.noreply.github.com>
2022-03-10 14:09:06 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const server = http.Server(app);
 
 setupwebsocket(server);
 
-mongoose.connect('mongodb+srv://VitorStack:gabriel2014@cluster0.14tg7.mongodb.net/omni10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://user:password@cluster0.14tg7.mongodb.net/omni10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
